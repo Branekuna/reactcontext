ea5cf27165112a6cf4c7dc1187f0fd5a9202f8b6
Branimir Kunstek <branekuna@gmail.com>
2020-11-17 13:15:32 +0100

    using a reducer to change reactContext

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -2,13 +2,13 @@ import { useState, useContext } from 'react';
 import { BookContext } from '../contexts/BookContext';
 
 export const NewBookForm = () => {
-  const { addBook } = useContext(BookContext);
+  const { dispatch } = useContext(BookContext);
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    addBook(title, author);
+    dispatch({ type: 'ADD_BOOK', book: { title, author } });
     setTitle('');
     setAuthor('');
   };
--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,22 +1,13 @@
-import { createContext, useState } from 'react';
-import { v4 } from 'uuid';
+import { createContext, useReducer } from 'react';
+import { BookReducer } from '../reducers/BookReducer';
 
 export const BookContext = createContext();
 
 export const BookContextProvider = (props) => {
-  const [books, setBooks] = useState([
-    { title: 'Name of the Wind', author: 'Patrick Rothfuss', id: 1 },
-    { title: 'The Final Empire', author: 'Brandon Sanderson', id: 2 },
-  ]);
-  const addBook = (title, author) => {
-    setBooks([...books, { title, author, id: v4.apply() }]);
-  };
-  const removeBook = (id) => {
-    setBooks(books.filter((book) => book.id !== id));
-  };
+  const [books, dispatch] = useReducer(BookReducer, []);
 
   return (
-    <BookContext.Provider value={{ books, addBook, removeBook }}>
+    <BookContext.Provider value={{ books, dispatch }}>
       {props.children}
     </BookContext.Provider>
   );
